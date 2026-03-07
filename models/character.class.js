c6d544087379359a1e92b70a88ee420563de9a16
TuncCu <tuncay-cubukcu@hotmail.de>
2026-03-07 11:49:36 +0100

    change hight and width of chicken and character

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject {
-    
+    height = 330;
+    width = 130;
+    y = 50;
 
     constructor(){
         super().loadImage('2_character_pepe/2_walk/W-21.png');
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MovableObject {
-    y = 290;
+    y = 300;
+    height = 70;
+    width = 90;
 
     constructor() {
         super().loadImage('3_enemies_chicken/chicken_normal/1_walk/1_w.png')
