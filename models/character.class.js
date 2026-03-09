4ec0d8fc6aba9dd4752de849fac1e4b7b2a44ff5
TuncCu <tuncay-cubukcu@hotmail.de>
2026-03-09 10:55:05 +0100

    character in animation

--- models/character.class.js
+++ models/character.class.js
@@ -2,9 +2,30 @@ class Character extends MovableObject {
     height = 330;
     width = 130;
     y = 50;
+    IMAGES_WALKING = [
+        '2_character_pepe/2_walk/W-21.png',
+        '2_character_pepe/2_walk/W-22.png',
+        '2_character_pepe/2_walk/W-23.png',
+        '2_character_pepe/2_walk/W-24.png',
+        '2_character_pepe/2_walk/W-25.png',
+        '2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
 
     constructor(){
         super().loadImage('2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length; 
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 250 )
     }
 
     jump() {
