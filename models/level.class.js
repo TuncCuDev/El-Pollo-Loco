ef94c8957314e0abd0ccfa900ccd441e418208d9
TuncCu <tuncay-cubukcu@hotmail.de>
2026-03-10 16:58:52 +0100

    created new class for level, new js for level1.js,

---
+++ models/level.class.js
@@ -0,0 +1,11 @@
+class Level {
+    enemies;
+    clouds;
+    Backgrounds;
+
+    constructor(enemies, clouds, Backgrounds) {
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.Backgrounds = Backgrounds;
+    }
+}
