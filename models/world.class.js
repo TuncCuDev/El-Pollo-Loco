2f718a0dff6260917482b664083187adf3c65d71
TuncCu <tuncay-cubukcu@hotmail.de>
2026-03-10 16:43:06 +0100

    added background images

--- models/world.class.js
+++ models/world.class.js
@@ -9,14 +9,25 @@ class World {
         new Cloud()
     ];
     Backgrounds = [
+        new Background('5_background/layers/air.png', -720),
+        new Background('5_background/layers/3_third_layer/2.png', -720),
+        new Background('5_background/layers/2_second_layer/2.png', -720),
+        new Background('5_background/layers/1_first_layer/2.png', -720),
+
         new Background('5_background/layers/air.png', 0),
         new Background('5_background/layers/3_third_layer/1.png', 0),
         new Background('5_background/layers/2_second_layer/1.png', 0),
-        new Background('5_background/layers/1_first_layer/1.png', 0)
+        new Background('5_background/layers/1_first_layer/1.png', 0),
+
+        new Background('5_background/layers/air.png', 720),
+        new Background('5_background/layers/3_third_layer/2.png', 720),
+        new Background('5_background/layers/2_second_layer/2.png', 720),
+        new Background('5_background/layers/1_first_layer/2.png', 720)
     ];
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -34,10 +45,14 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.Backgrounds)
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies)
+
+        this.ctx.translate(-this.camera_x, 0);
 
         // Draw wird immer wieder aufgerufen
         let self = this;
