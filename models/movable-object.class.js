7dd2042b0cc231574ac7d4d2823594e5287b7051
TuncCu <tuncay-cubukcu@hotmail.de>
2026-03-12 10:20:07 +0100

    is Colliding if else function, losing energy 100-2

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -10,6 +10,7 @@ class MovableObject {
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
+    energy = 100;
 
 
     applyGravity() {
@@ -45,6 +46,18 @@ class MovableObject {
         }
     }
 
+    // character.isColliding(chicken);
+    isColliding(mo) {
+        return this.x + this.width > mo.x &&
+            this.y + this.height > mo.y &&
+            this.x < mo.x &&
+            this.y < mo.y + mo.height
+    }
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image.png', 'img/image2.png', ...]
+     */
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
--- models/world.class.js
+++ models/world.class.js
@@ -12,10 +12,22 @@ class World {
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
+        this.checkCollisions();
     }
 
     setWorld() {
         this.character.world = this;
+    }
+
+    checkCollisions() {
+        setInterval(() => {
+            this.level.enemies.forEach((enemy) => {
+                if (this.character.isColliding(enemy)) {
+                    this.character.energy -= 2;
+                    console.log('Collision with Character, energy', this.character.energy);
+                };
+            })
+        }, 200);
     }
 
 
