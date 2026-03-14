e7f29689801083b8751b6dce56977ab9e67686b2
TuncCu <tuncay-cubukcu@hotmail.de>
2026-03-14 10:17:40 +0100

    created status bar for health

--- models/world.class.js
+++ models/world.class.js
@@ -5,13 +5,15 @@ class World {
     ctx;
     keyboard;
     camera_x = 0;
+    statusBar = new StatusBar();
 
+    
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
         this.keyboard = keyboard;
-        this.draw();
         this.setWorld();
+        this.draw();
         this.checkCollisions();
     }
 
@@ -24,20 +26,23 @@ class World {
             this.level.enemies.forEach((enemy) => {
                 if (this.character.isColliding(enemy)) {
                     this.character.hit();
-                    console.log('Collision with Character, energy', this.character.energy);
+                    this.statusBar.setPercentage(this.character.energy);
                 };
             })
         }, 200);
     }
 
-
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.ctx.translate(this.camera_x, 0);
-
         this.addObjectsToMap(this.level.Backgrounds);
 
+        this.ctx.translate(-this.camera_x, 0);
+        // --------Space for fixed objects
+        this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0); //Forwards
+        
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies)
